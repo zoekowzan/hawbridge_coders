3daf01aab68442825347a63ae6d8530d9400c119
Darin London <dmlond@users.noreply.github.com>
2015-12-15 13:58:48 -0500

    more semicolons and braces

--- public/js/hawbridge_coders.js
+++ public/js/hawbridge_coders.js
@@ -1,15 +1,16 @@
 var toggle_hideable = function() {
   elements=document.getElementsByClassName('hideable');
   for(i in elements){ 
-    this_element = elements[i]
-    if (!(this_element.style) || this_element.style.display === 'block' || this_element.style.display === '')
+    this_element = elements[i];
+    if (this_element.style.display === 'block' || this_element.style.display === '') {
       this_element.style.display = 'none';
       document.getElementById('toggle_hideable_button').innerHTML = 'Reveal Hidden Message';
-    else
+    }
+    else {
       this_element.style.display = 'block';
       document.getElementById('toggle_hideable_button').innerHTML = 'Hide Message';
   }
-}
+};
 
 window.onload = function(e) {
   toggle_hideable();
