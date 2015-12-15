43ec2586f0fd240f56b6b0adf61e0e97e80530c6
Darin London <dmlond@users.noreply.github.com>
2015-12-15 14:04:08 -0500

    bugfix

--- public/js/hawbridge_coders.js
+++ public/js/hawbridge_coders.js
@@ -2,17 +2,14 @@ var toggle_hideable = function() {
   elements=document.getElementsByClassName('hideable');
   for(i in elements){ 
     this_element = elements[i];
-    if (this_element.style.display === 'block' || this_element.style.display === '') {
+    if (this_element.style.display === 'block' || this_element.style.display === '')
       this_element.style.display = 'none';
-      document.getElementById('toggle_hideable_button').innerHTML = 'Reveal Hidden Message';
-    }
-    else {
+      document.getElementById('toggle_hideable_button').innerHTML = "Reveal Hidden Message";
+    else
       this_element.style.display = 'block';
-      document.getElementById('toggle_hideable_button').innerHTML = 'Hide Message';
-  }
+      document.getElementById('toggle_hideable_button').innerHTML = "Hide Message";
 }
 
 window.onload = function(e) {
   toggle_hideable();
 }
-
